04c63c9f807c42b7ea47a0ceff6c206759fb97e1
Kasi <57307447+jhlee225@users.noreply.github.com>
2020-02-06 17:10:51 +0900

    Solve : Valid Parentheses

---
+++ 20200206/Kasi.js
@@ -0,0 +1,18 @@
+var isValid = function(s) {
+    const START = ["\(","\{","\["]
+    const END = ["\)","\}","\]"]
+    let check = [];
+    for(i=0;i<s.length;i++){
+        if (START.indexOf(s[i]) >= 0 ){
+            check.push(s[i]);
+        } else if (START.indexOf(check.pop()) !== END.indexOf(s[i])){ 
+            return false;
+        }
+    }
+    
+    if(check.length==0){
+        return true
+    }else{
+        return false
+    }
+};
