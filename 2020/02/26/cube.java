711e69a1b8d9080a1040feaf0bae85fe27dd664c
juyoung-jeong <60762172+juyoung-jeong@users.noreply.github.com>
2020-02-27 11:23:47 +0900

    Create cube.java

---
+++ 2020/02/26/cube.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String [] split = s.split(" ");
+        if(split.length == 0){
+            return 0;
+        }
+        
+        return split[split.length-1].length();
+    }
+}
